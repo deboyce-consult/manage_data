693da194edfef015a3c86898e62e07f3c6d36c8d
donald e. boyce <donald.e.boyce@gmail.com>
2024-09-13 13:41:05 -0400

    top level directories under Data are packages

--- aps-2019-08/results/__init__.py
+++ aps-2019-08/results/__init__.py
@@ -3,6 +3,12 @@ from pathlib import Path
 
 import numpy as np
 
+from . import update_h5
 
-HERE = Path("__file__").parent
+HERE = Path(__file__).parent
 FF_DATA = HERE / "2024_09_06_lshr_r6.h5"
+SAMPLE = "sample_00"
+
+
+def get_data():
+    return update_h5.update(FF_DATA, SAMPLE)
